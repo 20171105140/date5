aae80d97bdd2c60d40c039853008fd320f5c0c24
20171105140 <s20171105140@mac02.local>
2018-07-09 13:15:42 +0800

    Initial Commit

---
+++ date5/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  date5
+//
+//  Created by 张学飞 on 18/7/9.
+//  Copyright © 2018年 张学飞. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
